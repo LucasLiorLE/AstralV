987a083fda3dcb197fa406babeaa82c40faad385
LucasLiorLE <LucasLiorLEYT@gmail.com>
2025-03-31 00:24:07 -0700

    Bruh I accidentally did im just farming commits

--- bot_utils/__init__.py
+++ bot_utils/__init__.py
@@ -8,7 +8,7 @@ from .utils import *
 VERSION = {
     'major': 2,
     'minor': 4,
-    'patch': 20,
+    'patch': 19,
 }
 
 __version__ = f"{VERSION['major']}.{VERSION['minor']}.{VERSION['patch']}"
